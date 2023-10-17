3fdf78b1b0dd7732d217482f3996fdc5afdbd8e4
jeffers9n <jeffersonlucassantos19@gmail.com>
2023-10-17 16:43:51 -0300

    conteudo atualizado para utilização do ESCAPE

--- aula 10.17/duas_strings.py
+++ aula 10.17/duas_strings.py
@@ -1,8 +1,11 @@
+# você pode ESCAPAR aspas duplas utilizando aspas simples
+# vale reforçar que isso só vale para aspas duplas,outros caracteres não.
 # texto1 recebe o valor "sou do"
 texto1 = "sou do"
 
+# caracter de SACAPE é o \
 # texto2 recebe o valor "back -end"
-texto2 = "back-end"
+texto2 = "\"back-end\""
 
 #a função print,contatena e imprime as duas variaveis na tela
 print (texto1, texto2)
--- aula 10.17/soma_variaveis.py
+++ aula 10.17/soma_variaveis.py
@@ -1,4 +1,4 @@
 valor1 =10
 valor2 =30
-soma =valor1 + valor2
-print(soma)
+soma =valor + valor2
+print("soma")
