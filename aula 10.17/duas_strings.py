752153ca600310aed1cecab896a7fe1076e71873
jeffers9n <jeffersonlucassantos19@gmail.com>
2023-10-17 16:29:27 -0300

    adicionado o comentario ao arquivo duas_strings.py

--- aula 10.17/duas_strings.py
+++ aula 10.17/duas_strings.py
@@ -1,4 +1,8 @@
+# texto1 recebe o valor "sou do"
 texto1 = "sou do"
+
+# texto2 recebe o valor "back -end"
 texto2 = "back-end"
 
+#a função print,contatena e imprime as duas variaveis na tela
 print (texto1, texto2)
