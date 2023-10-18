924a7d149b3d08d5cf20de6d9d1f17e71068d3d7
jeffers9n <jeffersonlucassantos19@gmail.com>
2023-10-18 14:30:09 -0300

    foi criado o arquivo não nomeados.py

---
+++ aula 10.18/nao_nomeados.py
@@ -0,0 +1,10 @@
+#valores nomeados
+a =1
+b =1
+soma =a + b
+print(soma)
+
+#valores não nomeados
+print (1 +1) 
+
+#obs:VALORES NÃO NOMEADOS, NÃO SE PROPAGAM NO SEU CODIGO.
--- aula10.16/hello_word.py
+++ aula10.16/hello_word.py
@@ -1,1 +1,11 @@
-print ("hello word")
+#texto1 recebe o valor de "gosto de"
+texto1 ="O pell"
+
+#texto2 recebe o valor de "programar"
+texto2 ="\"gosta\""
+#caracter do SACAPE é o \
+#texto2 = "\"gosto de-programar\""
+
+#A função print,contatena e imprime as duas variaveis na tela
+print(texto1,texto2,"de programar")
+
