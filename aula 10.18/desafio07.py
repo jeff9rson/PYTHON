60a220f11309d4bde6f98b26e8ce609da9615962
jeffers9n <jeffersonlucassantos19@gmail.com>
2023-10-20 16:06:41 -0300

    adicionado o arquivo desafio07.py

--- aula 10.18/desafio07.py
+++ aula 10.18/desafio07.py
@@ -3,4 +3,4 @@ numero2 = int( input ("informe o segundo numero") )
 numero3 = int( input ("informe o terceiro numero") )
 
 print ((numero1*2 * numero2/2) + numero3)
-print (numero1)
+print ((numero1 *3 + numero2)* numero3)
