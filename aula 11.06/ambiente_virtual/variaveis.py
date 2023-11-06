45360950c1d3b2bb6a989722c55adc4501dc9d28
jeffers9n <jeffersonlucassantos19@gmail.com>
2023-11-06 16:35:20 -0300

    arquivos e ambientes virtual adicionado.py

---
+++ aula 11.06/ambiente_virtual/variaveis.py
@@ -0,0 +1,11 @@
+#variaveis são usadas para salvar algo na memoria do computador
+#o sinal de = é utilizado para atribuição de valores
+var01 = "alguma coisa" 
+print(var01)
+var01 = "outra coisa"
+print(var01)
+var01 = 10
+print (var01)
+var02 = "A" + "B" + "C"
+print(var02)
+var02 = "A" * 10
